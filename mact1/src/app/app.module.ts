fb528337b559460b4077afeda8724c97fba1a83b
Ginel <ginelexaltado@gmail.com>
2018-01-09 23:15:51 +0800

    Declare Pipes

--- mact1/src/app/app.module.ts
+++ mact1/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title.component';
 import { UserService } from './user.service';
+import { AwesomePipe } from './awesome.pipe';
 
 
 @NgModule({
@@ -12,6 +13,10 @@ import { UserService } from './user.service';
   AppComponent,
   HighlightDirective,
   TitleComponent,
+
+  AwesomePipe,
+  ContactComponent,
+  ContactHighlightDirective
 ],
   imports: [ BrowserModule, FormsModule ],
   providers: [ ContactService, UserService ],
