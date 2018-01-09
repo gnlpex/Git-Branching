9befe18d6c09bb09d42bc2962baa05e189797ba8
Ginel <ginelexaltado@gmail.com>
2018-01-09 23:02:30 +0800

    NgModule provider example

--- mact1/src/app/app.module.ts
+++ mact1/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title.component';
+import { UserService } from './user.service';
 
 
 @NgModule({
@@ -15,7 +16,7 @@ import { TitleComponent } from './title.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
